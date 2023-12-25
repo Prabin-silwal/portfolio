a7b3cd964994c39d4443533682fe0eb6391d8b47
Prabin silwal <prabinsilwal@Prabins-MBP.lan>
2023-12-25 13:17:58 +1100

    added routing to the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,33 @@
-import React from 'react';
+
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
 
 import LandingPage from './Pages/HomePage/landingPage';
 
+
+function HomeScreen()
+{
+  return (
+    <div>
+     <LandingPage></LandingPage>
+    </div>
+  );
+}
+
 function App() {
   return (
-    <LandingPage/>
+   <div className="App">
+    <Router>
+      <Routes>
+      <Route path="/" element={<Home />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </Router>
+   </div>
   );
 }
 
--- src/Pages/Projects/View/ProjectView.tsx
+++ src/Pages/Projects/View/ProjectView.tsx
@@ -1,14 +1,13 @@
 import ProjectViewComponent from '../Utilities/projectViewComponent';
 import '../CSS/ProjectView';
-export default function ProjectView()
-{
+export default function ProjectView() {
     return (
-        <div  className="main-div"style={{ }}>
-    <div className='second-div'>
-    </div>
-    <div  className='third-div'>
-    </div>
-</div>
+        <div className="main-div" style={{}}>
+            <div className='second-div'>
+            </div>
+            <div className='third-div'>
+            </div>
+        </div>
 
     );
 }
